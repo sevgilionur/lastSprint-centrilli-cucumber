33d8863eac15c5928fae6bb31d6271a9ea6e2751
onur <sevgilionur25@hotmail.com>
2022-07-21 14:47:26 +0300

    First Commit

---
+++ src/test/java/com/centrilli/pages/BasePage.java
@@ -0,0 +1,22 @@
+package com.centrilli.pages;
+
+import com.centrilli.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "login")
+    public WebElement email;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement password;
+
+    @FindBy(css = ".btn-primary")
+    public WebElement loginButton;
+}
