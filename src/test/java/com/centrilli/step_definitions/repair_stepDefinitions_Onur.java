52a95d4da5b9571642650824600df6dcb52f6619
onur <sevgilionur25@hotmail.com>
2022-07-21 15:11:02 +0300

    Commit

---
+++ src/test/java/com/centrilli/step_definitions/repair_stepDefinitions_Onur.java
@@ -0,0 +1,21 @@
+package com.centrilli.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class repair_stepDefinitions_Onur {
+
+    @Given("User goes to application page and enter valid credentials onur")
+    public void user_goes_to_application_page_and_enter_valid_credentials_onur() {
+
+    }
+    @When("User clicks on repairs button onur")
+    public void user_clicks_on_repairs_button_onur() {
+
+    }
+    @Then("User lands on repairs orders page onur")
+    public void user_lands_on_repairs_orders_page_onur() {
+
+    }
+}
