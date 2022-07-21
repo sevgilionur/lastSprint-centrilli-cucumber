959817118f765b07e7fe1b0bb53b829e3f4cf339
onur <sevgilionur25@hotmail.com>
2022-07-21 15:59:36 +0300

    Hooks is created

---
+++ src/test/java/com/centrilli/step_definitions/Hooks.java
@@ -0,0 +1,25 @@
+package com.centrilli.step_definitions;
+
+import com.centrilli.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @After
+    public void teardownScenario(Scenario scenario) throws InterruptedException {
+
+        //if scenario fails take screenshot
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        //Thread.sleep(1000);
+        Driver.closeDriver();
+    }
+}
