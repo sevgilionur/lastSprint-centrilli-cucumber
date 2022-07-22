a25a4aa77a4047f289fbd72f9112f78d2f5c6ae4
Murat <laserdancer06@gmail.com>
2022-07-22 23:48:25 +0300

    Second commit to AC1 and AC2

--- src/test/java/com/centrilli/step_definitions/Repair_StepDefinitions_Murat.java
+++ src/test/java/com/centrilli/step_definitions/Repair_StepDefinitions_Murat.java
@@ -2,6 +2,7 @@ package com.centrilli.step_definitions;
 
 import com.centrilli.pages.RepairPage_Murat;
 import com.centrilli.utilities.Driver;
+import com.centrilli.utilities.Utilities;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -12,14 +13,31 @@ public class Repair_StepDefinitions_Murat {
 
     @When("User clicks on repairs button_murat")
     public void user_clicks_on_repairs_button_murat() throws InterruptedException {
-        Thread.sleep(3000);
+        Utilities.waitForClickablility(repairPage_murat.repairButton,3);
        repairPage_murat.repairButton.click();
         System.out.println("click on repair button");
 
     }
     @Then("User sees Repair Order Page_murat")
     public void user_sees_repair_order_page_murat() throws InterruptedException {
-       Thread.sleep(3000);
-        Assert.assertEquals("Repair Orders - Odoo", Driver.getDriver().getTitle());
+      Thread.sleep(3000);
+        // Assert.assertEquals("Repair Orders - Odoo", Driver.getDriver().getTitle());
+        Assert.assertTrue(Driver.getDriver().getTitle().startsWith("Repair Orders"));
     }
+    @When("User clicks existing {string} order_murat")
+    public void userClicksExistingOrder_murat(String productNumber) {
+        Utilities.waitForClickablility(repairPage_murat.getProductWebElement(productNumber),3);
+        repairPage_murat.getProductWebElement(productNumber).click();
+    }
+
+
+    @Then("User can access the existing {string} Repair Orders_murat")
+    public void userCanAccessTheExistingRepairOrders_murat(String productNumber) throws InterruptedException {
+        Thread.sleep(3000);
+        Assert.assertTrue(Driver.getDriver().getTitle().startsWith("RMA/"+productNumber));
+    }
+
+
+
+
 }
